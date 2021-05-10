e657bafbf8dd889e1617de3c7e1900af2a0f6a33
adaeo <wangj9507@gmail.com>
2021-05-10 18:50:56 +1000

    Added button functionality to userBtn.

--- client/Gui.java
+++ client/Gui.java
@@ -19,6 +19,14 @@ public class Gui {
         JButton userBtn = new JButton("USER");
         JButton adminBtn = new JButton("ADMIN");
 
+        userBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose(); // Close Login Frame
+                buildUser(); // Build User Frame
+            }
+        });
+
         JLabel userLabel = new JLabel("Username");
         JLabel pwdLabel = new JLabel("Password");
 
@@ -62,6 +70,42 @@ public class Gui {
 
         frame.pack(); // Leave size management to layout manager -- CardLayout
         frame.setResizable(false); // Disable resizing
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Terminate program on closure
+        frame.setVisible(true);
+
+    }
+
+    public static void buildUser() {
+        JFrame frame = new JFrame(); // Create holding frame
+
+        JPanel panelCont = new JPanel();
+        JPanel panel2 = new JPanel();
+        JPanel panel3 = new JPanel();
+        JButton button1 = new JButton("Switch to 2");
+        JButton button2 = new JButton("Switch to 1");
+
+        panel2.add(button1);
+        panel3.add(button2);
+
+        panel2.setBackground(Color.blue);
+        panel3.setBackground(Color.green);
+
+        panelCont.setLayout(cl);
+        panelCont.add(panel2, "panel2");
+        panelCont.add(panel3, "panel3");
+        cl.show(panelCont, "panel2");
+
+        button1.addActionListener(e -> cl.show(panelCont, "panel3"));
+        // These are both the same b1 and b2. One is done with lambda
+        button2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cl.show(panelCont, "panel2");
+            }
+        });
+
+        frame.add(panelCont);
+        frame.pack(); // Leave size management to layout manager -- CardLayout
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Terminate program on closure
         frame.setVisible(true);
 
