fcc0d3b82deb543f9aa98088705a4525cb901f0b
adaeo <wangj9507@gmail.com>
2021-05-10 17:32:47 +1000

    Switch GUI to Client Package

--- client/Gui.java
+++ client/Gui.java
@@ -1,4 +1,4 @@
-package server;
+package client;
 
 import javax.swing.*;
 import java.awt.*;
--- server/Main.java
+++ server/Main.java
@@ -1,13 +1,13 @@
 package server;
 
+import client.Gui;
+
 import javax.swing.*;
 
 public class Main {
 
     // main function.
     public static void main(String[] args) {
-
-        SwingUtilities.invokeLater(() -> Gui.buildLogin()); // Execute Runnable on AWT thread
 
     }
 }
