fb0cc58296112c9c2e47d52ab2871fabb81a7094
adaeo <wangj9507@gmail.com>
2021-05-11 22:39:27 +1000

    Added Organisation to database with foreign key to Account Table.

--- server/JDBCDatabaseSource.java
+++ server/JDBCDatabaseSource.java
@@ -6,32 +6,52 @@ public class JDBCDatabaseSource {
 
     public static final String CREATE_TABLE_ACCOUNT =
             "CREATE TABLE IF NOT EXISTS account ("
-                    + "userID INTEGER PRIMARY KEY /*!40101 AUTO_INCREMENT */ NOT NULL UNIQUE," // from https://stackoverflow.com/a/41028314
+                    + "userID INTEGER /*!40101 AUTO_INCREMENT */ NOT NULL UNIQUE," // from https://stackoverflow.com/a/41028314
                     + "userName VARCHAR(30) NOT NULL UNIQUE,"
                     + "password CHAR(128) NOT NULL,"
-                    + "unit CHAR(50) NOT NULL"
+                    + "unitName VARCHAR(60) NOT NULL,"
+                    + "PRIMARY KEY (userID),"
+                    + "FOREIGN KEY (unitName) REFERENCES organisation(unitName)"
                     + ");";
 
-    private static final String INSERT_USER = "INSERT INTO account (userName, password, unit) VALUES (?, ?, ?);";
+    private static final String INSERT_ACCOUNT = "INSERT INTO account (userName, password, unitName) VALUES (?, ?, ?);";
     private static final String GET_ACCOUNT = "SELECT * FROM account WHERE userName=?";
+
+
+
+    public static final String CREATE_TABLE_ORGANISATION =
+            "CREATE TABLE IF NOT EXISTS organisation ("
+                    + "unitID INTEGER /*!40101 AUTO_INCREMENT */ NOT NULL UNIQUE,"
+                    + "unitName VARCHAR(60) NOT NULL UNIQUE,"
+                    + "budget INTEGER NOT NULL,"
+                    + "PRIMARY KEY (unitName)"
+                    + ")";
+
+    private static final String INSERT_UNIT = "INSERT INTO organisation (unitName, budget) VALUES (?, ?);";
+    private static final String GET_UNIT = "SELECT * FROM organisation WHERE unitName=?";
 
 
     private Connection connection;
 
     private PreparedStatement addAccount;
     private PreparedStatement getAccount;
+    private PreparedStatement addOrganisation;
+    private PreparedStatement getOrganisation;
 
     public JDBCDatabaseSource() {
         connection = JDBCConnection.getConnection();
 
         try {
             Statement st = connection.createStatement();
+            st.execute(CREATE_TABLE_ORGANISATION);
             st.execute(CREATE_TABLE_ACCOUNT);
-            addAccount = connection.prepareStatement(INSERT_USER);
+            addAccount = connection.prepareStatement(INSERT_ACCOUNT);
             getAccount = connection.prepareStatement(GET_ACCOUNT, ResultSet.TYPE_SCROLL_INSENSITIVE);
+            addOrganisation = connection.prepareStatement(INSERT_UNIT);
+            getOrganisation = connection.prepareStatement(GET_UNIT, ResultSet.TYPE_SCROLL_INSENSITIVE);
 
-        } catch (SQLException sqle) {
-            sqle.printStackTrace();
+        } catch (SQLException SQLex) {
+            System.out.println(SQLex);
         }
     }
 
@@ -40,12 +60,13 @@ public class JDBCDatabaseSource {
             /* BEGIN MISSING CODE */
             addAccount.setString(1, a.getUsername());
             addAccount.setString(2, a.getPassword());
-            addAccount.setString(3, a.getUnit());
+            addAccount.setString(3, getOrganisation(a.getUnit())[1]);
             addAccount.execute();
             /* END MISSING CODE */
-        } catch (SQLException ex) {
-            System.out.println("Account with username " + a.getUsername()
-            + " already exists.");
+        } catch (SQLException SQLex) {
+            System.out.println(SQLex);
+        } catch (NullPointerException NPex) {
+            System.out.println(NPex);
         }
     }
 
@@ -62,11 +83,46 @@ public class JDBCDatabaseSource {
             rs.first();
             user = rs.getString("userName");
             pwd = rs.getString("password");
-            unit = rs.getString("unit");
+            unit = rs.getString("unitName");
             account = new String[] {user, pwd, unit};
             return account;
-        } catch (SQLException ex) {
-            ex.printStackTrace();
+        } catch (SQLException SQLex) {
+            System.out.println(SQLex);
+        }
+        return null;
+    }
+
+    public void addOrganisation(Organisation a) {
+        try {
+            /* BEGIN MISSING CODE */
+            addOrganisation.setString(1, a.getName());
+            addOrganisation.setString(2, String.valueOf(a.getBudget()));
+            addOrganisation.execute();
+            /* END MISSING CODE */
+        } catch (SQLException SQLex) {
+            //SQLex.printStackTrace();
+            System.out.println(SQLex);
+        }
+    }
+
+    public String[] getOrganisation(String name) {
+        String[] organisation;
+        String unitID;
+        String unitName;
+        String budget;
+        ResultSet rs;
+
+        try {
+            getOrganisation.setString(1, name);
+            rs = getOrganisation.executeQuery();
+            rs.first();
+            unitID = rs.getString("unitID");
+            unitName = rs.getString("unitName");
+            budget = rs.getString("budget");
+            organisation = new String[] {unitID, unitName, budget};
+            return organisation;
+        } catch (SQLException SQLex) {
+            System.out.println(SQLex);
         }
         return null;
     }
--- server/Main.java
+++ server/Main.java
@@ -7,20 +7,30 @@ import javax.swing.*;
 public class Main {
 
     private static User test;
+    private static User test2;
 
     private static Organisation unitTest;
+    private static Organisation unitTest1; // Not in Database
 
     private static JDBCDatabaseSource database;
 
     // main function.
     public static void main(String[] args) {
 
-        unitTest = new Organisation("unit1", 10000);
-        test = new User("adaeo", "secret", unitTest);
-
         database = new JDBCDatabaseSource();
 
+        unitTest = new Organisation("unit1", 10000);
+        database.addOrganisation(unitTest);
+
+        unitTest1 = new Organisation("unit2", 9999);
+
+        // This will work because unitTest organisation is in the database!!
+        test = new User("adaeo", "secret", unitTest);
         database.addAccount(test);
+
+        // This will not work because unitTest1 organisation is not in the database!!
+        test2 = new User("william", "secret", unitTest1);
+        database.addAccount(test2);
 
         String[] account = database.getAccount("adaeo");
 
@@ -29,6 +39,14 @@ public class Main {
         String thisUnit = account[2];
 
         System.out.println(thisUser + " " + thisPwd + " " + thisUnit);
+
+        String[] unit = database.getOrganisation("unit1");
+
+        String unitID = unit[0];
+        String unitName = unit[1];
+        String unitBudget = unit[2];
+
+        System.out.println(unitID + " " + unitName + " " + unitBudget);
 
         database.closeDatabaseSource();
     }
